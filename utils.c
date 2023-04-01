aee0f1e3a38815d18eef2abf7e4fbce035383f32
Opeyemi Oduyemi <119660096+oduyemi@users.noreply.github.com>
2023-04-01 16:22:54 +0100

    Update utils.c

--- utils.c
+++ utils.c
@@ -16,23 +16,23 @@ int is_printable(char c)
 
 /**
  * append_hexa_code - Append ascci in hexadecimal code to buffer
- * @buffer: Array of chars.
+ * @peepee: Array of chars.
  * @i: Index at which to start appending.
  * @ascii_code: ASSCI CODE.
  * Return: Always 3
  */
-int append_hexa_code(char ascii_code, char buffer[], int i)
+int append_hexa_code(char ascii_code, char peepee[], int i)
 {
 	char map_to[] = "0123456789ABCDEF";
 	/* The hexa format code is always 2 digits long */
 	if (ascii_code < 0)
 		ascii_code *= -1;
 
-	buffer[i++] = '\\';
-	buffer[i++] = 'x';
+	peepee[i++] = '\\';
+	peepee[i++] = 'x';
 
-	buffer[i++] = map_to[ascii_code / 16];
-	buffer[i] = map_to[ascii_code % 16];
+	peepee[i++] = map_to[ascii_code / 16];
+	peepee[i] = map_to[ascii_code % 16];
 
 	return (3);
 }

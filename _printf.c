9ed777827833d6e6701d18b2941c1a2873ab6ce9
Opeyemi Oduyemi <119660096+oduyemi@users.noreply.github.com>
2023-04-01 16:06:12 +0100

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -12,7 +12,7 @@ int _printf(const char *format, ...)
 	int i, printed = 0, printed_chars = 0;
 	int flags, width, precision, size, buff_ind = 0;
 	va_list list;
-	char buffer[BUFF_SIZE];
+	char peepee[BUFF_SIZE];
 
 	if (format == NULL)
 		return (-1);
@@ -23,29 +23,29 @@ int _printf(const char *format, ...)
 	{
 		if (format[i] != '%')
 		{
-			buffer[buff_ind++] = format[i];
+			peepee[buff_ind++] = format[i];
 			if (buff_ind == BUFF_SIZE)
-				print_buffer(buffer, &buff_ind);
+				print_peepee(peepee, &buff_ind);
 			/* write(1, &format[i], 1);*/
 			printed_chars++;
 		}
 		else
 		{
-			print_buffer(buffer, &buff_ind);
+			print_peepee(peepee, &buff_ind);
 			flags = get_flags(format, &i);
 			width = get_width(format, &i, list);
 			precision = get_precision(format, &i, list);
 			size = get_size(format, &i);
 			++i;
-			printed = handle_print(format, &i, list, buffer,
+			done = handle_print(format, &i, list, buffer,
 				flags, width, precision, size);
-			if (printed == -1)
+			if (done == -1)
 				return (-1);
-			printed_chars += printed;
+			printed_chars += done;
 		}
 	}
 
-	print_buffer(buffer, &buff_ind);
+	print_peepee(peepee, &buff_ind);
 
 	va_end(list);
 
@@ -53,14 +53,14 @@ int _printf(const char *format, ...)
 }
 
 /**
- * print_buffer - Prints the contents of the buffer if it exist
- * @buffer: Array of chars
+ * print_peepee - Prints the contents of the buffer if it exist
+ * @peepee: Array of chars
  * @buff_ind: Index at which to add next char, represents the length.
  */
-void print_buffer(char buffer[], int *buff_ind)
+void print_peepee(char peepee[], int *buff_ind)
 {
 	if (*buff_ind > 0)
-		write(1, &buffer[0], *buff_ind);
+		write(1, &peepee[0], *buff_ind);
 
 	*buff_ind = 0;
 }
